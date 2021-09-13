a3ae87703f4891389e5889ecd41195c4ad1e3d87
zaharia93 <84892237+zaharia93@users.noreply.github.com>
2021-09-13 10:11:47 +0300

    .

--- js/carusel.js
+++ js/carusel.js
@@ -2,22 +2,13 @@ var slides = $('.slide');
 var dot = $('.slide-dot');
 var $ = jQuery;
 
-// Javascript for image slider manual navigation
+
 var changeSlide = function(index){
   slides.removeClass('active');
   dot.removeClass('active');
   slides.eq(index).addClass('active');
   dot.eq(index).addClass('active');
 }
-
-// var changeSlide = function(index)  {
-//   slides.removeClass("active");
-//   slides.eq(index).addClass("active");
-//   dot.removeClass("active");
-//   dot.eq(index).addClass("active");
-//   console.log("asd");
-// };
-
 
 var init = function( ) {
   $(".slide-dot").on("click", function() {
@@ -37,30 +28,3 @@ $(document).bind("DOMSubtreeModified", function( ){
 });
 
 
-// Javascript for image slider autoplay navigation
-// var repeat = function(activeClass){
-//   let active = document.getElementsByClassName('active');
-//   let i = 1;
-
-//   var repeater = () => {
-//     setTimeout(function(){
-//       [...active].forEach((activeSlide) => {
-//         activeSlide.classList.remove('active');
-//       });
-
-//     slides[i].classList.add('active');
-//     btns[i].classList.add('active');
-//     i++;
-
-//     if(slides.length == i){
-//       i = 0;
-//     }
-//     if(i >= slides.length){
-//       return;
-//     }
-//     repeater();
-//   }, 10000);
-//   }
-//   repeater();
-// }
-// repeat();
