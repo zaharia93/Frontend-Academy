d7c217b38cc4754c19168cea44ae37aac2449888
zaharia93 <84892237+zaharia93@users.noreply.github.com>
2021-09-30 12:06:46 +0300

    .

--- js/header.js
+++ js/header.js
@@ -1,15 +1,28 @@
-var toggleNav = $(".btn.menu-btn");
-var navLinks = $(".nav-links");
-var closeNavBtn = $(".nav-links .close-btn");
+const toggleNav = $(".btn.menu-btn");
+const navLinks = $(".nav-links");
+const closeNavBtn = $(".nav-links .close-btn");
+const bodyEl = $("body");
+const nav = $(".navigation-wrapper");
 
 toggleNav.on("click", function() {
     if (navLinks.hasClass("mobile-nav-active")) {
         navLinks.removeClass("mobile-nav-active");
+        bodyEl.css("overflow", "");
     } else {
         navLinks.addClass("mobile-nav-active");
+        bodyEl.css("overflow", "hidden");
     }
 });
 
 closeNavBtn.on("click", function() {
     navLinks.removeClass("mobile-nav-active");
+    bodyEl.css("overflow", "");
+});
+
+$(window).on("scroll", function() {
+    if ($(this).scrollTop() > 1) {
+        nav.addClass("sticky-nav");
+    } else {
+        nav.removeClass("sticky-nav");
+    }
 });
--- js/pdp.js
+++ js/pdp.js
@@ -1,6 +1,6 @@
 //Change picture
 
-const imgs = document.querySelectorAll('.img-select .img-item');
+const imgs = document.querySelectorAll(".img-select .img-item");
 const imgBtns = [...imgs];
 let imgId = 1;
 
@@ -16,41 +16,42 @@ let imgId = 1;
 const imgsThumbnails = $(".img-select .img-item");
 
 imgsThumbnails.on("click", function () {
-    var $this = $(this);
-    imgsThumbnails.removeClass("selected-img");
-    $(this).addClass("selected-img");
-    imgId = $this.data("id");
-    slideImage();
+  var $this = $(this);
+  imgsThumbnails.removeClass("selected-img");
+  $(this).addClass("selected-img");
+  imgId = $this.data("id");
+  slideImage();
+});
 
-})
+function slideImage() {
+  const displayWidth = document.querySelector(
+    ".img-showcase img:first-child"
+  ).clientWidth;
 
-function slideImage(){
-    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
-    
-    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+  document.querySelector(".img-showcase").style.transform = `translateX(${
+    -(imgId - 1) * displayWidth
+  }px)`;
 }
 
-window.addEventListener('resize', slideImage);
-
+window.addEventListener("resize", slideImage);
 
 //Read more
-
-
-$(document).ready(function(){
-    var maxLength = 300;
-    $(".show-read-more").each(function(){
-        var myStr = $(this).text();
-        if($.trim(myStr).length > maxLength){
-            var newStr = myStr.substring(0, maxLength);
-            var removedStr = myStr.substring(maxLength, $.trim(myStr).length);
-            $(this).empty().html(newStr);
-            $(this).append(' <a href="javascript:void(0);" class="read-more">Read more</a>');
-            $(this).append('<span class="more-text">' + removedStr + '</span>');
-        }
-    });
-    $(".read-more").click(function(){
-        $(this).siblings(".more-text").contents().unwrap();
-        $(this).remove();
-    });
+$(document).ready(function () {
+  var maxLength = 300;
+  $(".show-read-more").each(function () {
+    var myStr = $(this).text();
+    if ($.trim(myStr).length > maxLength) {
+      var newStr = myStr.substring(0, maxLength);
+      var removedStr = myStr.substring(maxLength, $.trim(myStr).length);
+      $(this).empty().html(newStr);
+      $(this).append(
+        ' <a href="javascript:void(0);" class="read-more">Read more</a>'
+      );
+      $(this).append('<span class="more-text">' + removedStr + "</span>");
+    }
+  });
+  $(".read-more").click(function () {
+    $(this).siblings(".more-text").contents().unwrap();
+    $(this).remove();
+  });
 });
-  
